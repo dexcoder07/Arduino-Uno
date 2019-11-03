05ce87231421242a8c02227dc5b3315cbcd15249
dexcoder07 <55050907+dexcoder07@users.noreply.github.com>
2019-11-03 21:06:07 +0530

    Add files via upload

---
+++ Exp_7_CLCD/Dislplay Welcome to chandigarh university.ino
@@ -0,0 +1,17 @@
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  lcd.print("Welcome to Chandigarh University");  
+  }
